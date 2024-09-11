86089a2c144cf3c386714313f65397587f55b1a3
junior <>
2024-09-11 22:42:32 +0200

    Add explanation comment to `check.cmd`

--- check.cmd
+++ check.cmd
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S 2>/dev/null=2>NUL sh -x
 
+: : This is a polyglot that works both in Windows 'cmd' and Unix 'sh'. The shebang however relies on flags specific to GNU 'env'.
+
 mypy --no-error-summary . || exit 1
 ruff check --quiet . || exit 1
 ruff format --diff --quiet . || exit 1
